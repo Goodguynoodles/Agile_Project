ab3794e9ba777c5d088ec54863d2307903b65546
Goodguynoodles <koosvanlosenoord@gmail.com>
2021-01-21 13:21:33 +0100

    game_page_added

---
+++ Agile/content/game.php
@@ -0,0 +1,23 @@
+<div class="container">
+    <div class="row">
+        <div class="col-12 game letter-size custom-border font">
+            game
+        </div>
+        <div class="col-4 game-info custom-border font">
+            <h1>game info</h1>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.
+        </div>
+        <div class="col-1"></div>
+        <div class="col-3 game-info custom-border">
+            <h1>why game here</h1>
+        </div>
+        <div class="col-1"></div>
+        <div class="col-3 filler game-info custom-border">
+            <h1>what you can win</h1>
+        </div>
+    </div>
+</div>
--- Agile/index.php
+++ Agile/index.php
@@ -18,6 +18,8 @@
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <!-- Eigen stylesheet -->
     <link rel="stylesheet" href="./css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
     <!-- Tabblad Naam -->
     <title>Test</title>
 </head>
