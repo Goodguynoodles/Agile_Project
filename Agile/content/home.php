d52f08a55fb20caf9c8e6ab1a96e995d923d8b7c
thijsieboy007 <56019364+thijsieboy007@users.noreply.github.com>
2021-01-21 16:10:35 +0100

    Add files via upload

--- Agile/content/home.php
+++ Agile/content/home.php
@@ -1,46 +1,19 @@
-<div class="hero-image">
-  <div class="hero-text">
-    <p class="font-yeon-sung">JAAR VAN DE OS</p>
-    <h1 class="font-yeon-sung" style="font-size:50px">CHINEES NIEUWJAAR 2021</h1>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-  </div>
-</div>
 
-
-
-
-<nav class="navbar navbar-expand-lg bg-white custom-border">
-    <div class="container">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar10">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="navbar-collapse collapse" id="navbar10">
-            <ul class="navbar-nav nav-fill w-100">
-                <li class="nav-item">
-                     <h6 class="color-red font-chivo">WEBSHOP</h6>
-                </li>
-                <li class="nav-item">
-                     <h6 class="color-red font-chivo">WEBSHOP</h6>
-                </li>
-                <li class="nav-item">
-                     <h6 class="color-red font-chivo">WEBSHOP</h6>
-                </li>
-                <li class="nav-item">
-                      <h6 class="color-red font-chivo">WEBSHOP</h6>
-                </li>
-                <li class="nav-item">
-                      <h6 class="color-red font-chivo">WEBSHOP</h6>
-                </li>
-            </ul>
+<div class="container hometext">
+    <div class="row">
+        <div class="col-8">
+        <h1>Chinees nieuwjaar</h1>
+            <p>Welkom op de site van chinees nieuwjaar!</p>
+            <p>Op deze website kan je alle informatie vinden over chinees nieuwjaar.</p>
+            <p>Druk op de knoppen boven in om naar andere pagina's te gaan en om de website</p>
+            <p>extra leuk te maken hebben we ook een leuk spel gemaakt in de stijl van chinees niewjaar</p>
+            <p>2021!</p>
+            <p>Deze site en game is gemaakt door 8 studenten van het mbo utrecht voor mensen die graag meer</p>
+            <p>Over dit onderwerp willen weten. Dit is ook waarom wij erg ons best hierop hebben gedaan.</p>
+        </div>
+        <div class="col-4">
+            <img src="./img/home.png">
         </div>
     </div>
-</nav>
+</div>
+
