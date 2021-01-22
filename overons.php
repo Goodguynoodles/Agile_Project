9fd5b70e74ad83a69eaf0b1e1cb55756618bad83
completenoob-creator <54887747+completenoob-creator@users.noreply.github.com>
2021-01-22 06:06:29 +0100

    over ons gemaakt en een style er bij gemaakt

---
+++ overons.php
@@ -0,0 +1,70 @@
+<div>
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1 class="display-4">wie wij zijn</h1>
+            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent a lobortis libero, a
+                convallis sapien. Donec at consequat ante, at finibus nulla. Cras ac suscipit odio. Maecenas luctus
+                rhoncus felis, eget placerat mi faucibus eu. Suspendisse sollicitudin, enim eget porttitor finibus, nibh
+                purus condimentum sem, a faucibus lectus arcu vitae leo. Nullam lacinia pellentesque euismod. In
+                placerat in leo in finibus. Suspendisse eleifend libero a purus faucibus sodales. Duis vel efficitur
+                dui. Praesent eget viverra metus, mollis luctus nibh. Mauris quis maximus elit. Suspendisse aliquam
+                hendrerit nulla sit amet fermentum. Proin fringilla risus eu orci sodales, eget vehicula diam
+                condimentum. Morbi convallis eros gravida, pulvinar libero vitae, vestibulum ex. Sed eleifend malesuada
+                velit vitae aliquam.</p>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="card col-sm tussen" style="width: 18rem;">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-text">Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus.</p>
+                </div>
+            </div>
+            <div class="card col-sm tussen" style="width: 18rem;">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-text">Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus.</p>
+                </div>
+            </div>
+            <div class="card col-sm tussen" style="width: 18rem;">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-text">Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus.</p>
+                </div>
+            </div>
+            <div class="card col-sm tussen" style="width: 18rem;">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-text">Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="card col-sm tussen" style="width: 18rem;">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-text">Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus.</p>
+                </div>
+            </div>
+            <div class="card col-sm tussen" style="width: 18rem;">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-text">Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus.</p>
+                </div>
+            </div>
+            <div class="card col-sm tussen" style="width: 18rem;">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-text">Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus.</p>
+                </div>
+            </div>
+            <div class="card col-sm tussen" style="width: 18rem;">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-text">Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
