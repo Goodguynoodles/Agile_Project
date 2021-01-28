2118266469e77a2fb1503e6798d93d0ce83118de
Goodguynoodles <koosvanlosenoord@gmail.com>
2021-01-28 15:38:22 +0100

    over ons added

--- Agile/content/overons.php
+++ Agile/content/overons.php
@@ -1,1 +1,8 @@
-
+<div class="container">
+    <div class="row">
+        <div class="col-6 custom-border font"><h2>Waarom deze website</h2>Met deze website willen we uw graag infromeren over het cineese nieuw jaar het jaar van de os.
+        en we willen uw informeren door een info pagina en een game die onze desiners met moeite gemaakt heeft voor uw om licht te breggen op het jaar van de os.</div>
+        <div class="col-1"></div>
+        <div class="col-5 custom-border font"><h2>Wie zijn wij</h2>wij zijn een klijne groep of develepors die een mensen wouden informeren over het cinees nieuw jaar</div>
+    </div>
+</div>
