e3d8db2c57c41b2583227b7c3a3be35eece13ad9
thijsieboy007 <56019364+thijsieboy007@users.noreply.github.com>
2021-01-28 17:09:39 +0100

    Update home.php

--- Agile/content/home.php
+++ Agile/content/home.php
@@ -1,19 +1,21 @@
-
-<div class="container hometext">
+<div class="container">
     <div class="row">
-        <div class="col-8">
+        <div class="col-6 custom-border font">
         <h1>Chinees nieuwjaar</h1>
-            <p>Welkom op de site van chinees nieuwjaar!</p>
-            <p>Op deze website kan je alle informatie vinden over chinees nieuwjaar.</p>
-            <p>Druk op de knoppen boven in om naar andere pagina's te gaan en om de website</p>
-            <p>extra leuk te maken hebben we ook een leuk spel gemaakt in de stijl van chinees niewjaar</p>
-            <p>2021!</p>
-            <p>Deze site en game is gemaakt door 8 studenten van het mbo utrecht voor mensen die graag meer</p>
-            <p>Over dit onderwerp willen weten. Dit is ook waarom wij erg ons best hierop hebben gedaan.</p>
+        <p>Welkom op de site van chinees nieuwjaar!
+            Op deze website kan je alle informatie vinden over chinees nieuwjaar.
+            Druk op de knoppen boven in om naar andere pagina's te gaan en om de website
+            extra leuk te maken hebben we ook een leuk spel gemaakt in de stijl van chinees niewjaar
+            2021!</p>
+            <p>
+            Deze site en game is gemaakt door 8 studenten van het mbo utrecht voor mensen die graag meer
+            Over dit onderwerp willen weten. Dit is ook waarom wij erg ons best hierop hebben gedaan.</p>
+            <p>Een kleine samevating van Chinees nieuwjaar is dat het een grote feestdag in China is om het 
+            nieuwe jaar te vieren is die elk jaar een nieuwe datum heeft. Het is dus niet zoals bij normaal nieuwjaar dat 
+            het altijd op een datum van het jaar is.</p>
         </div>
-        <div class="col-4">
-            <img src="./img/home.png">
-        </div>
+        <div class="col-1"></div>
+        <div class="col-5"><img src="./img/home.png"></div>
     </div>
 </div>
 
