f929151c098405df3780c7d440c000abddcde965
Goodguynoodles <koosvanlosenoord@gmail.com>
2021-01-29 01:17:09 +0100

    game_op_website

--- Agile/content/game.php
+++ Agile/content/game.php
@@ -1,23 +1,23 @@
 <div class="container">
     <div class="row">
-        <div class="col-12 game letter-size custom-border font">
-            game
+        <div class="col-1"></div>
+        <div class="col-9 letter-size custom-border">
+            <iframe src="https://i.simmer.io/@Goodguynoodles/osrunner" style="width:960px;height:600px"></iframe>
         </div>
+        <div class="col-2"></div>
         <div class="col-4 game-info custom-border font">
             <h1>game info</h1>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-            anim id est laborum.
+            onze game is een infinite runner over een os die zo ver mogelijk wilt komen.
         </div>
         <div class="col-1"></div>
         <div class="col-3 game-info custom-border">
             <h1>why game here</h1>
+            de reden waarom deze game is hier is omdat een metafor is voor de race die de os heeft gedaan om zijn plek in de race te krijgen.
         </div>
         <div class="col-1"></div>
         <div class="col-3 filler game-info custom-border">
             <h1>what you can win</h1>
+            de persoon die de hoogste score heeft kijt gratis bier op het festival.
         </div>
     </div>
 </div>
